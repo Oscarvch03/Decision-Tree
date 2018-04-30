1e68076644579533893a793ab5a601582c78b974
Oscarvch <oscar.velasco@urosario.edu.co>
2018-04-29 21:19:37 -0500

    First Delivery

---
+++ project.py
@@ -0,0 +1,27 @@
+class Arbol:
+    def nodo(rotulo, [0,1] , iz , der):
+        Arbol = nodo("A", A1, A2)
+        A1 = nodo("A1",[0,0.5], A11, A12) 
+        A11 = nodo("A11",[0,0.25), null, null)
+        A12 = nodo("A12",[0.25,0.5], null, null)
+        A2 = nodo("A2",[0.5,1], A21, A22)
+        A21 = nodo("A21",[0.5,0.75], null, null)
+        A22 = nodo("A22", [0.75,1], null, null)
+
+    for 
+
+
+
+        
+    def_init_(self, animal = None, clasificacion=None, izquierda =None, derecha=None):
+        self.animal = animal
+        self.clasificacion = clasificacion
+        self.izquierda = izquierda
+        self.derecha = derecha
+
+    def_str_(self):
+        return "%s %s" %(self.animal, self clasificacion)
+
+
+
+        
