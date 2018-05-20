4e8ec71ede521e1c1af0e98ca0e41936c1a046b5
Natalia565 <angela.oviedo@urosario.edu.co>
2018-05-20 11:31:56 -0500

    Graphics win and lose

--- project2.py
+++ project2.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 ######################################################################################
 
 #Librerias Importadas
@@ -7,9 +6,8 @@ import random
 import matplotlib.pyplot as plt
 
 ######################################################################################
-##### Definicion de objetos y Funciones
 
-# Define los objetos arboles
+# Definir los objetos arboles
 
 class Tree(object):
     def __init__(self, izq, der, rotulo, intervalo):
@@ -18,14 +16,54 @@ class Tree(object):
         self.rotulo = rotulo
         self.intervalo = intervalo
 
-def imprimir_arbol(A):
-    # Imprime un arbol en notacion [ [] [] ]
-    if A.izq == None:
-        return str(A.rotulo)
+######################################################################################
+
+# Definir las funciones
+
+def crear_objetos(NoObjetos):
+
+    # Crea objetos (numeros flotantes) al azar entre 0 y 1, y los inserta
+    # En una lista
+
+    objetos = []
+    for i in range(NoObjetos):
+        objetos.append(random.uniform(0,1))
+    return objetos
+
+
+def crear_contexto(NoObjetos, TamContexto):
+
+    # Selecciona objetos a partir de la lista Objetos
+    # devuelve una lista nueva con los objetos seleccionados
+
+    return random.sample(NoObjetos, TamContexto)
+
+def buscar(nodo, valor):
+
+    # Revisa que rotulo se le asigna a un objeto
+
+    print "Buscando en categoria: " + str(nodo.rotulo) + " con el valor: " + str(valor)
+    if nodo.izq != None:
+        if (valor > nodo.izq.intervalo[0]) and (valor < nodo.izq.intervalo[1]):
+             print "Chinga la cosa a la izquierda"
+             return buscar(nodo.izq, valor)
+        else:
+            print "Ay caramba"
+            if nodo.der != None:
+                if (valor > nodo.der.intervalo[0]) and (valor < nodo.der.intervalo[1]):
+                     print "Chinga la cosa a la derecha"
+                     return buscar(nodo.der, valor)
+                else:
+                     print "Ay caramba, estamos en la mala"
     else:
-        return "[" + imprimir_arbol(A.izq) + ", " + imprimir_arbol(A.der) + "]"
+        print "Lo que buscas es: " + str(nodo.rotulo)
+        return nodo.rotulo
 
 def insertar(nodo, valor):
+
+    # Inserta los objetos seleccionados en la funcion crear_contexto, clasificandolos
+    # en el arbol de discriminacion
+
     if nodo.izq != None:
         if (valor > nodo.izq.intervalo[0]) and (valor < nodo.izq.intervalo[1]):
             print "Esta a la izq"
@@ -39,48 +77,12 @@ def insertar(nodo, valor):
         nodo.der = Tree(None, None, nodo.rotulo + "2", [(nodo.intervalo[0] + nodo.intervalo [1])/2 , nodo.intervalo[1]])
         return nodo
 
-
-def crear_objetos(NoObjetos):
-    # for i in range("NoObjetos"):
-    #   anhadir a la lista un objeto aleatorio
-    objetos = []
-    for i in range(NoObjetos):
-        objetos.append(random.uniform(0,1))
-    return objetos
-    #depende del numero de objetos
-
-
-def buscar(nodo, valor):
-    # print "Buscando en categoria: " + str(nodo.rotulo) + " con el valor: " + str(valor)
-    if nodo.izq != None:
-        if (valor > nodo.izq.intervalo[0]) and (valor < nodo.izq.intervalo[1]):
-             # print "Chinga la cosa a la izquierda"
-             return buscar(nodo.izq, valor)
-        else:
-            # print "Ay caramba"
-            if nodo.der != None:
-                if (valor > nodo.der.intervalo[0]) and (valor < nodo.der.intervalo[1]):
-                     # print "Chinga la cosa a la derecha"
-                     return buscar(nodo.der, valor)
-                # else:
-                #     print "Ay caramba, estamos en la mala"
-    else:
-        # print "Lo que buscas es: " + str(nodo.rotulo)
-        return nodo.rotulo
-
-
-def crear_contexto(NoObjetos, TamContexto):
-    # Selecciona cinco objetos a partir de la lista Objetos
-    # Se sobreentiende que el primer objeto sera el foco
-    # devuelve una lista de objetos
-    return random.sample(NoObjetos, TamContexto)
-
-
 def juego_discriminacion(nodo, Contexto):
-    # Recibe un arbol y una lista de cinco objetos (contexto)
-    # y devuelve True si el rotulo de contexto[0] es diferente
-    # del rotulo de los demas objetos del contexto. Devuelve
+
+    # Recibe un arbol y una lista de cinco objetos (contexto) y devuelve True si el rotulo
+    # de contexto[0] es diferente del rotulo de los demas objetos del contexto. Devuelve
     # False en otro caso.
+
     print "El contexto es: ", Contexto
     rotulos = []
     for o in Contexto:
@@ -96,12 +98,23 @@ def juego_discriminacion(nodo, Contexto):
         return True
 
 def Rondas_100(Ronda, Objetos):
-    # Ronda: cantidad de rondas para jugar el juego de discriminacion
-    # Objetos: Los objetos del mundo con los cuales se arman los contextos
-    #
+
+    # Ejecuta la funcion juego_discriminacion determinadas veces para evaluar la eficiencia
+    # de aprendizaje del arbol de discriminacion
+    # Si la maquina gana (es decir logra asignarle un rotulo a un unico objeto), entonces al
+    # contador Heroe se le suma 1
+    # Si la maquina pierde (es decir se le asigna mas de un objeto a un rotulo), entonces al
+    # contador Asistente se le suma 1
+    # A la vez el estado actual por ronda de cada contador se agrega a una lista respectiva,
+    # y el ultimo numero de cada lista es la cantidad de veces que la maquina gano y perdio
+
     Heroe = 0
+    Asistente = 0
     victorias = []
+    derrotas = []
+
     A = Tree(None, None, "A", [0.,1.])
+
     for n in range(Ronda):
         contexto = crear_contexto(Objetos, TamContexto)
         print "Vamos a correr el juego con el arbol: ", imprimir_arbol(A)
@@ -109,79 +122,64 @@ def Rondas_100(Ronda, Objetos):
             Heroe += 1
         else:
             A = insertar(A, contexto[0])
-
+            Asistente += 1
         victorias.append(Heroe)
-    return A, victorias
+        derrotas.append(Asistente)
+    return A, victorias, derrotas
 
-######################################################################################
+def imprimir_arbol(A):
 
+    # Imprime el arbol de cada ronda de juego_discriminacion en notacion [ [] [] ]
 
-# Ejemplo de un arbolBinario
-# Nodos
-# A222 = Tree(None, None, "A222",[0.875, 1])
-# A221 = Tree(None, None, "A221",[0.75, 0.875])
-#
-# A212 = Tree(None, None, "A212",[0.625, 0.75])
-# A211 = Tree(None, None, "A211",[0.5, 0.625])
-#
-# A122 = Tree(None, None, "A122",[0.375, 0.5])
-# A121 = Tree(None, None, "A121",[0.25, 0.375])
-#
-# A112 = Tree(None, None, "A112",[0.125, 0.25])
-# A111 = Tree(None, None, "A111",[0, 0.125])
-#
-# A22 = Tree(A221, A222, "A22",[0.75, 1])
-# A21 = Tree(A211, A212, "A21",[0.5, 0.75])
-#
-# A12 = Tree(A121, A122, "A12",[0.25, 0.5])
-# A11 = Tree(A111, A112, "A11",[0, 0.25])
-#
-# A12 = Tree(None, None, "A12",[0.25, 0.5])
-# A11 = Tree(None, None, "A11",[0, 0.25])
-#
-# A22 = Tree(None, None, "A22",[0.75, 1])
-# A21 = Tree(None, None, "A21",[0.5, 0.75])
-#
-# A2 = Tree(A21, A22, "A2",[0.5, 1])
-# A1 = Tree(A11, A12, "A1",[0, 0.5])
-#
-# A = Tree(A1, A2, "A", [0, 1])
-#
-# print imprimir_arbol(A)
-
-# A = (None, None, "A", [0, 1])
-# A = insertar(A)
-# A.izq = insertar(A.izq)
-
+    if A.izq == None:
+        return str(A.rotulo)
+    else:
+        return "[" + imprimir_arbol(A.izq) + ", " + imprimir_arbol(A.der) + "]"
 
 ######################################################################################
 
 # PARAMETROS DEL MODELO
-NoObjetos = 20
-TamContexto = 5
-Ronda = 100
+
+# NoObjetos: Los objetos del mundo con los cuales se arman los contextos
+# TamContexto: Los objetos seleccionados al azar de la lista de NoObjetos
+# Ronda: cantidad de rondas para jugar el juego de discriminacion
+
+NoObjetos = 10
+TamContexto = 3
+Ronda = 10
 
 ######################################################################################
 
-# Invocar Funciones
+# Invocar las funciones principales
 
-# Buscar = buscar(A, 0.3)
-# print Buscar
-#
-# ##Insertar = insertar(A, 0.3)
-# ##print Insertar
-#
 Objetos = crear_objetos(NoObjetos)
 print Objetos
 
+Arb, vict , derr = Rondas_100(Ronda, Objetos)
 
-Arb, Succ = Rondas_100(Ronda, Objetos)
-print Succ
-print "El porcentaje de victorias es: " + str(float(Succ[-1])/Ronda*100) + "%"
+# Imprimir el porcentaje de veces que la maquina perdio o gano el juego
 
+print vict
+print "El porcentaje de victorias es: " + str(float(vict[-1])/Ronda*100) + "%"
+print derr
+print "El porcentaje de derrotas es: " + str(float(derr[-1])/Ronda*100) + "%"
 
-f, axarr = plt.subplots(1)
-axarr.set_ylabel('Victorias Acumuladas')
-axarr.set_xlabel('Ronda')
-axarr.plot(Succ)
+######################################################################################
+
+# Graficas (Rondas ganadas vs Total Rondas) && (Rondas perdidas vs Total Rondas)
+
+a, b = plt.subplots()
+b.set_ylabel('Victorias Acumuladas')
+b.set_xlabel('Ronda')
+b.plot(vict)
+
+plt.hold(True)
+
+c, d = plt.subplots()
+d.set_ylabel('Derrotas Acumuladas')
+d.set_xlabel('Ronda')
+d.plot(derr)
+
 plt.show()
+
+######################################################################################
